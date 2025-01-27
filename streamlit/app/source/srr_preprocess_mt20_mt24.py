62c1d36a29a00872defb99b8c4ff50b4954ef8f6
AntoinePELAMOURGUES <antoinepela@gmail.com>
2025-01-27 11:02:40 +0400

    :hammer: Update columns integration

--- streamlit/app/source/srr_preprocess_mt20_mt24.py
+++ streamlit/app/source/srr_preprocess_mt20_mt24.py
@@ -15,12 +15,22 @@ def preprocess_data(file1, file2):
     filtered_columns_2 = list(set(expected_columns_file2) & set(available_columns_2))
     df1 = df1[filtered_columns_1]
     df2 = df2[filtered_columns_2]
-    if 'Date' in df1.columns:
-        df1['Date'] = pd.to_datetime(df1['Date'])
+    df = df1.merge(df2, on="CIREF", how="left")
+    deleted_columns =['Critère Recherché_x', 'Commentaire_x', '3ème interlocuteur', 'IMEI', 'IMSI', 'Nature Correspondant',
+       'Nature 3ème interlocuteur', 'GCI_x', 'EGCI_x', 'NGCI_x', 'Code PLMN',
+       'Volume de données montant', 'Volume de données descendant',"Opérateur d'itinérance", 'Indicateur RO', 'Décalage horaire',
+       'Service de Base', 'IPV4 VO Wifi', 'IPV6 VO Wifi',
+       'Port Source VO Wifi', 'Critère Recherché_y', 'Commentaire_y', 'GCI_y',
+       'EGCI_y', 'NGCI_y', 'Système', 'Nom du site', 'Code zone', 'Coordonnée Z', 'Début asso. CIREF/GCI', 'Fin asso. CIREF/GCI']
+    for i in df.columns.tolist():
+        if i in deleted_columns:
+            df.drop(i, axis=1, inplace=True)
+    if 'Date' in df.columns:
+        df['Date'] = pd.to_datetime(df['Date'])
         # Extraire l'année, le mois et le jour de la semaine
-        df1['Années'] = df1['Date'].dt.year
-        df1['Mois'] = df1['Date'].dt.month
-        df1['Jour de la semaine'] = df1['Date'].dt.day_name()
+        df['Années'] = df['Date'].dt.year
+        df['Mois'] = df['Date'].dt.month
+        df['Jour de la semaine'] = df['Date'].dt.day_name()
         # Mapper les jours de la semaine en français
         jours_semaine_fr = {
             'Monday': 'Lundi',
@@ -47,35 +57,37 @@ def preprocess_data(file1, file2):
             12: 'Décembre'
         }
         # Remplacer les numéros de mois par leur équivalent en français
-        df1['Mois'] = df1['Mois'].map(mois_fr)
+        df['Mois'] = df['Mois'].map(mois_fr)
         # Remplacer les noms des jours par leur équivalent en français
-        df1['Jour de la semaine'] = df1['Jour de la semaine'].map(jours_semaine_fr)
-    if 'Abonné' in df1.columns:
-        df1['Abonné'] = df1['Abonné'].replace(r'^0693', '262693', regex=True)
-        df1['Abonné'] = df1['Abonné'].replace(r'^0692', '262692', regex=True)
-        df1['Abonné'] = df1['Abonné'].replace(r'^06', '336', regex=True)
-    if 'Correspondant' in df1.columns:
-        df1['Correspondant'] = df1['Correspondant'].fillna('Data')
-        df1['Correspondant'] = df1['Correspondant'].str.split(',').str[0]
-        df1['Correspondant'] = df1['Correspondant'].replace(r'^0693', '262693', regex=True)
-        df1['Correspondant'] = df1['Correspondant'].replace(r'^0692', '262692', regex=True)
-        df1['Correspondant'] = df1['Correspondant'].replace(r'^06', '336', regex=True)
-        df1['Correspondant'] = df1['Correspondant'].replace(r'^07', '337', regex=True)
-        df1['Correspondant'] = df1['Correspondant'].replace(r'^02', '2622', regex=True)
-    if 'Bureau Distributeur' in df2.columns:
-        df2 = df2.rename(columns={'Bureau Distributeur': 'Ville'})
-        df2['Ville']= df2['Ville'].str.upper()
-        df2['Ville']= df2['Ville'].str.replace("-", " ")
-        df2['Ville']= df2['Ville'].str.replace("SAINT", "ST")
-        df2['Ville']= df2['Ville'].str.replace("SAINTE", "STE")
-        df2['Ville']= df2['Ville'].str.replace("L'", "")
-        df2['Ville'] = df2['Ville'].str.replace("É", "E", regex=False)
-    if 'Adresse' in df2.columns and 'Comp. adresse' in df2.columns and 'Code postal' in df2.columns:
-        df2['adresse_complete'] = df2['Adresse'] + " " + df2['Comp. adresse'] + " " + df2['Code postal'] + " " + df2['Ville']
-        df2['adresse_complete']= df2['adresse_complete'].str.upper()
-        df2['adresse_complete']= df2['adresse_complete'].str.replace(r'\s+', ' ', regex=True)
-        df2 = df2.drop(columns=['Adresse', 'Comp. adresse', 'Code postal'])
-        df2= df2.rename(columns={'adresse_complete': 'Adresse'})
-    df = df1.merge(df2, how="left", left_on="CIREF", right_on='CIREF')
+        df['Jour de la semaine'] = df['Jour de la semaine'].map(jours_semaine_fr)
+    if 'Abonné' in df.columns:
+        df['Abonné'] = df["Abonné"].ffill()
+        df['Abonné'] = df["Abonné"].bfill()
+        df['Abonné'] = df['Abonné'].replace(r'^0693', '262693', regex=True)
+        df['Abonné'] = df['Abonné'].replace(r'^0692', '262692', regex=True)
+        df['Abonné'] = df['Abonné'].replace(r'^06', '336', regex=True)
+    if 'Correspondant' in df.columns:
+        df['Correspondant'] = df['Correspondant'].fillna('Data')
+        df['Correspondant'] = df['Correspondant'].str.split(',').str[0]
+        df['Correspondant'] = df['Correspondant'].replace(r'^0693', '262693', regex=True)
+        df['Correspondant'] = df['Correspondant'].replace(r'^0692', '262692', regex=True)
+        df['Correspondant'] = df['Correspondant'].replace(r'^06', '336', regex=True)
+        df['Correspondant'] = df['Correspondant'].replace(r'^07', '337', regex=True)
+        df['Correspondant'] = df['Correspondant'].replace(r'^02', '2622', regex=True)
+    if 'Bureau Distributeur' in df.columns:
+        df = df.rename(columns={'Bureau Distributeur': 'Ville'})
+        df['Ville']= df['Ville'].str.upper()
+        df['Ville']= df['Ville'].str.replace("-", " ")
+        df['Ville']= df['Ville'].str.replace("SAINT", "ST")
+        df['Ville']= df['Ville'].str.replace("SAINTE", "STE")
+        df['Ville']= df['Ville'].str.replace("L'", "")
+        df['Ville'] = df['Ville'].str.replace("É", "E", regex=False)
+    if 'Adresse' in df.columns and 'Comp. adresse' in df.columns and 'Code postal' in df.columns:
+        df['adresse_complete'] = df['Adresse'] + "," + df['Code postal'] + " " + df['Ville']
+        df['adresse_complete']= df['adresse_complete'].str.upper()
+        df['adresse_complete']= df['adresse_complete'].str.replace(r'\s+', ' ', regex=True)
+        df.fillna({'adresse_complete': 'INDETERMINE'}, inplace=True)
+        df = df.drop(columns=['Adresse', 'Comp. adresse', 'Code postal'])
+        df= df.rename(columns={'adresse_complete': 'Adresse'})
     return df
 
